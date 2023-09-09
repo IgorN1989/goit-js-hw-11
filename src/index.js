04d0e534788f2acb77461704c9c84231e31e75d1
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-09-09 20:35:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-console.log(1111);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { fetchImages } from './js/api-pixabay';
+import { createMarkup } from './js/markup';
+
+// ========================================================
+
+const elements = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+const optionsForLightBox = {
+  captionsData: 'alt',
+  captionDelay: 250,
+  download: true,
+};
+
+const galleryLightbox = new SimpleLightbox('.gallery a', optionsForLightBox);
+
+elements.form.addEventListener('submit', onSubmitForm);
+
+async function onSubmitForm(evt) {
+  evt.preventDefault();
+  elements.gallery.innerHTML = '';
+
+  const searchValue = new FormData(evt.currentTarget).get('searchQuery');
+  if (!searchValue) {
+    Notify.warning('Please enter query for search');
+    return;
+  }
+
+  try {
+    const images = await fetchImages(searchValue);
+    console.log(images);
+    if (!images.length) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    Notify.info(`Hooray! We found ${totalHits} images.`);
+    elements.gallery.innerHTML = createMarkup(images);
+  } catch (err) {
+    Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
+  } finally {
+    galleryLightbox.refresh();
+    elements.form.reset();
+  }
+}
