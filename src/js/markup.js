2775f19b0235ff0f0ee5d29de2d3c49bae359475
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-09-10 13:39:31 +0300

    Update markup.js

--- src/js/markup.js
+++ src/js/markup.js
@@ -23,13 +23,13 @@ function createMarkup(arr) {
     </div>
     <div class="info">
         <p class="info-item">
-            <b>Likes</b> ${likes || '-'}
-        </p>
-        <p class="info-item">
-            <b>Views</b> ${views || '-'}
+            <b>Likes</b>${likes || '-'}
         </p>
         <p class="info-item">
             <b>Comments</b>${comments || '-'}
+        </p>
+        <p class="info-item">
+            <b>Views</b>${views || '-'}
         </p>
         <p class="info-item">
             <b>Downloads</b>${downloads || '-'}
