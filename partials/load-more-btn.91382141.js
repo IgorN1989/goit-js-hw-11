9628f0d4c7bb0714985ad16f1a9c7542572a6d16
IgorN1989 <107757685+IgorN1989@users.noreply.github.com>
2023-09-10 10:40:40 +0000

    Deploying to gh-pages from @ IgorN1989/goit-js-hw-11@7615893be9bc6a0b3d4e9e1be2c62768de25630e 🚀

---
+++ partials/load-more-btn.91382141.js
@@ -0,0 +1,2 @@
+window.onscroll=()=>function(){const n=document.querySelector(".search-container"),o=n.offsetHeight;window.scrollY>o?n.classList.add("no-transparency"):n.classList.remove("no-transparency")}();
+//# sourceMappingURL=load-more-btn.91382141.js.map
---
+++ partials/load-more-btn.de302fd6.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){return n=document.querySelector(".search-container"),o=n.offsetHeight,void(window.scrollY>o?n.classList.add("no-transparency"):n.classList.remove("no-transparency"));var n,o};
+//# sourceMappingURL=load-more-btn.de302fd6.js.map
